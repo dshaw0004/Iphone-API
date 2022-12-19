c641ba193df93f87d316fd4a30dd3be5411236a4
DShaw0004 <114005178+dshaw0004@users.noreply.github.com>
2022-12-19 21:53:47 +0530

    iphones js added

---
+++ iphones.js
@@ -0,0 +1,123 @@
+const iphones ={
+	no_of_products : 13,
+	info : [
+	{
+		id: 1,
+		model: "iPhone 14 Pro Max",
+		image:
+			"https://www.91-img.com/pictures/146919-v4-apple-iphone-14-pro-max-mobile-phone-large-1.jpg?tr=q-80",
+		price: 139900,
+
+		rating: 5,
+	},
+	{
+		id: 2,
+		model: "iPhone 14 Pro",
+		image:
+			" https://www.91-img.com/pictures/144646-v4-apple-iphone-14-pro-mobile-phone-medium-1.jpg?tr=q-80",
+		price: 129900,
+
+		rating: 5,
+	},
+	{
+		id: 3,
+		model: "iPhone 14 Plus",
+		image:
+			"https://www.91-img.com/pictures/151394-v4-apple-iphone-14-plus-mobile-phone-medium-1.jpg?tr=q-80",
+		price: 98400,
+	
+	},
+	{
+		id: 4,
+		model: "iPhone 14",
+		image:
+			"https://www.91-img.com/pictures/152564-v3-apple-iphone-14-256gb-mobile-phone-medium-1.jpg?tr=q-80",
+
+		price: 88400,
+
+		rating: 5,
+	},
+	{
+		id: 5,
+		model: "iPhone 14 Mini",
+		image:
+			"https://www.91-img.com/pictures/146914-v1-apple-iphone-14-mini-mobile-phone-medium-1.jpg?tr=q-80",
+		price: 74990,
+
+		rating: 5,
+	},
+	{
+		id: 6,
+		model: "iPhone 13 Pro Max",
+		image:
+			"https://www.91-img.com/pictures/142100-v5-apple-iphone-13-pro-max-mobile-phone-medium-1.jpg?tr=q-80",
+		price: 11990,
+
+		rating: 5,
+	},
+	{
+		id: 7,
+		model: "iPhone 13 Pro",
+		image:
+			"https://www.91-img.com/pictures/142099-v3-apple-iphone-13-pro-mobile-phone-medium-1.jpg?tr=q-80",
+		price: 111900,
+
+		rating: 5,
+	},
+	{
+		id: 8,
+		model: "iPhone 13",
+		image:
+			"https://www.91-img.com/pictures/140876-v3-apple-iphone-13-mobile-phone-medium-1.jpg?tr=q-80",
+		price: 65900,
+	
+		rating: 5,
+	},
+	{
+		id: 9,
+		model: "iPhone 13 Mini",
+		image:
+			"https://www.91-img.com/pictures/144021-v3-apple-iphone-13-mini-mobile-phone-medium-1.jpg?tr=q-80",
+		price: 64900,
+	
+		rating: 5,
+	},
+	{
+		id: 10,
+		model: "iPhone 12 Pro Max",
+		image:
+			"https://www.91-img.com/pictures/136141-v5-apple-iphone-12-pro-max-mobile-phone-medium-1.jpg?tr=q-80",
+		price: 10990,
+	
+		rating: 5,
+	},
+	{
+		id: 11,
+		model: "iPhone 12 Pro",
+		image:
+			"https://www.91-img.com/pictures/136140-v5-apple-iphone-12-pro-mobile-phone-medium-1.jpg?tr=q-80",
+		price: 100030,
+	
+		rating: 5,
+	},
+	{
+		id: 12,
+		model: "iPhone 12",
+		image:
+			"https://www.91-img.com/pictures/136139-v4-apple-iphone-12-mobile-phone-medium-1.jpg?tr=q-80",
+		price: 48999,
+	
+		rating: 5,
+	},
+	{
+		id: 13,
+		model: "iPhone 12 Mini",
+		image:
+			"https://www.91-img.com/pictures/140365-v6-apple-iphone-12-mini-mobile-phone-medium-1.jpg?tr=q-80",
+		price: 59900,
+		
+		rating: 5,
+	},
+]};
+
+module.exports = iphones;
